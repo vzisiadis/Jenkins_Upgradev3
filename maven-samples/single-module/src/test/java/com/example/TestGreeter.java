bae9be1ca6cfb86972ba76e89706189a3890a696
vzisiadis <vzisiadis@gmail.com>
2020-04-27 21:07:48 +0300

    updated tetcode.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+      @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you for everything";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
